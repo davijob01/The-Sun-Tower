86e1511fec5be9c57f9bea4bba1b8a4497661da4
Davi Job <davi.job@hotmail.com>
2022-11-11 23:25:20 -0300

    11/11
    adicionadas algumas animações e organizados alguns sistemas

--- The Sun Tower/Assets/Scripts/Player/PlayerAnimation.cs
+++ The Sun Tower/Assets/Scripts/Player/PlayerAnimation.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+    public PlayerScript playerScript;
+
     public Animator playerAnimator;
 
     private void Update()
     {
+        //WALKING ANIMATION CHECK
+
         if(Input.GetAxis("Horizontal") >= 0.2 || Input.GetAxis("Horizontal") <= -0.2) playerAnimator.SetBool("isWalking", true);
 
         else playerAnimator.SetBool("isWalking", false);
+
+        //ATTACK ANIMATION CHECK
+
+        if (playerScript.atackUp) playerAnimator.SetTrigger("attackUp");
+
+        else if (playerScript.atackdown) playerAnimator.SetTrigger("attackDown");
     }
 }
